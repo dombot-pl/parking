4ed6d4e086fab6eff4c299ea4fc56e2acb91649f
tom <tom@sapletta.com>
2020-11-18 21:56:13 +0100

    first version

---
+++ server.js
@@ -0,0 +1,5 @@
+// Start Express web app based on Express
+// const web = require('./src/web');
+// TODO: create npm package for this server
+const web = require('./server/express');
+return new web('parking.dombot.pl', 'localhost',80);
