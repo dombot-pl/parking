0a2744007dfbd78cfdcb981a3586a3f6e9c35388
tom <tom@sapletta.com>
2020-11-19 00:43:21 +0100

    update bugfix

--- public/js/domain.js
+++ public/js/domain.js
@@ -1,6 +1,12 @@
-var url = new URL(window.location.href);
-var domain = url.searchParams.get("host");
-//console.log(domain);
-document.getElementById("domain").value = domain;
-document.getElementById("title").innerHTML = domain;
+setTimeout(
+    function () {
+        var url = new URL(window.location.href);
+        var domain = url.searchParams.get("host");
+        //console.log(domain);
+        document.getElementById("domain").value = domain;
+        document.getElementById("title").innerHTML = domain;
+    }
+    , 300);
+
+
 
