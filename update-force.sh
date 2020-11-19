325922e69886a13811e3eea69d135bec79183859
tom <tom@sapletta.com>
2020-11-19 10:46:27 +0100

    update script restart

--- update-force.sh
+++ update-force.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "I will update git repository ..."
-git checkout master
-git branch new-branch-to-save-current-commits
-git fetch --all
+#git checkout master
+#git fetch --all
 git reset --hard origin/master
+git pull
